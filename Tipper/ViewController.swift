e79531e8091e2dcd705fdc491ad3874d3d030de4
Kingston Lee <kingston@chartboost.com>
2016-09-29 17:05:44 -0700

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Kingston Lee on 9/29/16.
+//  Copyright © 2016 Kingston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
